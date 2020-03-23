e616328fbe70292fc7ae96363b0a951b489f214a
Dimitar Katsarski <d.katsarski@outlook.com>
2020-03-23 19:27:39 +0200

    initial commit

---
+++ Empolyees-Analyzer/Empolyees-Analyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Empolyees_Analyzer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
