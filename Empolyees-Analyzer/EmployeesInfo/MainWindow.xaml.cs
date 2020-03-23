03f4fb184650ce56bf99605c3074f56ca36f6ab6
Dimitar Katsarski <d.katsarski@outlook.com>
2020-03-23 23:11:28 +0200

    datagrid done binding successfull

---
+++ Empolyees-Analyzer/EmployeesInfo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeesInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var emp = new Employee()
+            {
+                Id = 23
+            };
+
+            EmployeesDataGrid.Items.Add(emp);
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+        }
+    }
+}
