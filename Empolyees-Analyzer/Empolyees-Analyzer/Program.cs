32451fcd6b322727eb8b29c29ccdbf98244749cb
Dimitar Katsarski <d.katsarski@outlook.com>
2020-03-23 20:26:32 +0200

    reading from file -> done

--- Empolyees-Analyzer/Empolyees-Analyzer/Program.cs
+++ Empolyees-Analyzer/Empolyees-Analyzer/Program.cs
@@ -6,7 +6,12 @@ namespace EmpolyeesAnalyzer
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\dkats\Desktop\myText.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
         }
     }
 }
